c8fdc8ea31c99b820ba24403b994bd73fefd05d6
Julian Furchert <julian.furchert@raytion.com>
2020-10-29 23:18:52 +0100

    Update position

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { PrettyPaper } from '../thumbnails/PrettyPaper'
 export default function Home() {
   return (
     <Container>
-      <Flex css={{ justifyContent: 'space-between', alignItems: 'center' }}>
+      <Flex css={{ justifyContent: 'center', alignItems: 'center' }}>
         <PrettyPaper css={{ width: '50%' }} />
       </Flex>
     </Container>
