cc8ddd9e451b09c30bde78c645924b469cff1ca7
Julian Furchert <julian.furchert@raytion.com>
2020-10-29 10:55:08 +0100

    Update shadows

--- stitches.config.ts
+++ stitches.config.ts
@@ -68,8 +68,6 @@ const theme = {
     $small: '0 5px 10px rgba(0,0,0,0.12)',
     $medium: '0 8px 30px rgba(0,0,0,0.12)',
     $large: '0 30px 60px rgba(0,0,0,0.12)',
-    $hover: '0 30px 60px rgba(0,0,0,0.12)',
-    $sticky: '0 12px 10px -10px rgba(0,0,0,0.12)',
   }
 }
 
