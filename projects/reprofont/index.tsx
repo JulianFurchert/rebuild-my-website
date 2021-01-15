1490f2e1b0c01bbd74ac49bfcbdb6b10b17066aa
jf <julian.furchert@raytion.com>
2021-01-15 16:57:36 +0100

    Update Reprofont

--- projects/reprofont/index.tsx
+++ projects/reprofont/index.tsx
@@ -1,4 +1,7 @@
-import { Container, Text, Box, Paragraph } from '../../components'
+import { Container, Text, Box, Paragraph, Row, VideoCase, ComponentCase } from '../../components'
+
+import GraphicNew from './components/GraphicNew';
+import GraphicEEE from './components/GraphicEEE';
 
 export default function Framo() {
   return (
@@ -12,6 +15,29 @@ export default function Framo() {
         </Paragraph>
       </Box>
       <Box css={{marginTop: 60}}>
+        <Row>
+          <VideoCase 
+            video="/video/reprofont-100-mobil.mp4"
+            width={750}
+            height={1334}
+            maxWidth={80}
+          />
+          <ComponentCase maxWidth={80} mockup='none'>
+            <GraphicEEE />
+          </ComponentCase>
+        </Row>
+        <Row>
+          <ComponentCase maxWidth={80} mockup='none'>
+            <GraphicNew />
+          </ComponentCase>
+
+          <VideoCase 
+            video="/video/reprofont-300-mobil.mp4"
+            width={750}
+            height={1334}
+            maxWidth={80}
+          />
+        </Row>
       </Box>
     </Container>
   )
