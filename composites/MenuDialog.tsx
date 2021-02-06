5d04577211b89f83e196eae373a7e588fc4cb126
jf <julian.furchert@raytion.com>
2021-02-06 23:24:24 +0100

    update animation

--- composites/MenuDialog.tsx
+++ composites/MenuDialog.tsx
@@ -81,7 +81,7 @@ const MenuDialog = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.2 }}
+            transition={{ ease: 'easeInOut', duration: 0.1 }}
           />
         </Dialog.Overlay>
         <StyledContent>
@@ -90,7 +90,7 @@ const MenuDialog = () => {
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
+            transition={{ ease: 'easeInOut', duration: 0.3 }}
           >
             <Link onClick={handleOnClick} href="/" variant="menu">
               <Coffee size={20} /> 
