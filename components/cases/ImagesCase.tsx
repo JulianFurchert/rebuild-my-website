5447a5815491ce729158ecfd044c3aa5181e30c8
jf <julian.furchert@raytion.com>
2021-02-20 17:17:00 +0100

    seading reponsive

--- components/cases/ImagesCase.tsx
+++ components/cases/ImagesCase.tsx
@@ -2,41 +2,80 @@ import React from 'react';
 import Image from 'next/image' 
 import { Box } from '../Box' 
 import { Flex } from '../Flex' 
+import { useWindowSize } from "@reach/window-size";
+
+type ColumnsCount = number
+
+type Rows = ColumnsCount[]
+
+type Index = number
+
+type IndexRow = Index[]
+
+type IndexRows = IndexRow[]
 
 type Image = {
   src: string,
   height: number,
-  width: number
+  width: number,
 }
 
 type ImagesCaseProps = {
-  images: Image[][]
+  images: Image[],
+  rows: {
+    bp1?: Rows,
+    bp2?: Rows,
+    initial: Rows
+  }
 }
 
 export const ImagesCase: React.FC<ImagesCaseProps> = ({ 
   images,
+  rows: rowsProps,
   ...props 
 }) =>  {
+  const { width } = useWindowSize();
+  let rows = rowsProps.initial;
+
+  if(width <= 800 && rowsProps.bp2){
+    rows = rowsProps.bp2
+  }
+
+  if(width <= 760 && rowsProps.bp1){
+    rows = rowsProps.bp1
+  }
+
+  const indexRows: IndexRows = rows.reduce((acc: IndexRows, row) => {
+    let indexRow: IndexRow = [];
+    let lastRow = acc[acc.length - 1] || [-1];
+    let lastIndex = lastRow[lastRow.length - 1];
+
+    for (let i = lastIndex + 1; i < lastIndex + 1 + row; i++) {
+      indexRow.push(i)
+    }
+
+    return [...acc, indexRow]
+  }, []);
 
   return (
     <Box {...props} css={{ margin: '15px 0'}}>
-        {images.map(row => (
+        {indexRows.map(row => (
           <Flex style={{  margin: '0 -5px' }}>
-            {row.map(img => (
+            {row.map(i => images[i] && (
               <Box
                 css={{
                   backgroundColor: '$gray1',
                   margin: '$1'
                 }} 
                 style={{ 
-                  flex: `calc(${img.width}/${img.height})`,
+                  flex: `calc(${images[i].width}/${images[i].height})`,
                 }}
                 >
                 <Image
-                  src={img.src}
+                  src={images[i].src}
                   sizes="30vw"
-                  height={img.height}
-                  width={img.width}
+                  height={images[i].height}
+                  width={images[i].width}
                   layout="responsive"
                 />
               </Box>
--- projects/seading/index.tsx
+++ projects/seading/index.tsx
@@ -23,58 +23,56 @@ export default function Framo() {
       </Box>
       <Box css={{marginTop: 40}}>
         <ImagesCase 
+          rows={{
+            initial: [3,3,3],
+            bp1: [2,2,2,2],
+          }}
           images={[
-            [
-              {
-                src: '/img/seading/P9100123.jpg',
-                width: 4608,
-                height: 3456
-              },
-              {
-                src: '/img/seading/IMG_1904.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/IMG_1867.jpg',
-                width: 4898,
-                height: 3265
-              },
-            ],
-            [
-              {
-                src: '/img/seading/IMG_1848.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/IMG_1798.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/IMG_1790.jpg',
-                width: 4898,
-                height: 3265
-              },
-            ],
-            [
-              {
-                src: '/img/seading/IMG_1786.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/IMG_1738.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/DSCF4802.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ]
+            {
+              src: '/img/seading/P9100123.jpg',
+              width: 4608,
+              height: 3456
+            },
+            {
+              src: '/img/seading/IMG_1904.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/IMG_1867.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/IMG_1848.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/IMG_1798.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/IMG_1790.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/IMG_1786.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/IMG_1738.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/DSCF4802.jpg',
+              width: 4896,
+              height: 3264
+            },
           ]}
         />
         <Slideshow 
@@ -117,58 +115,56 @@ export default function Framo() {
           ]}
         />
         <ImagesCase 
+          rows={{
+            initial: [3,3,3],
+            bp1: [2,2,2,2],
+          }}
           images={[
-            [
-              {
-                src: '/img/seading/DSCF4789.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4765.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4753.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ],
-            [
-              {
-                src: '/img/seading/DSCF4741.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4737.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4699.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ],
-            [
-              {
-                src: '/img/seading/DSCF4692.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4631.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4582.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ]
+            {
+              src: '/img/seading/DSCF4789.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4765.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4753.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4741.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4737.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4699.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4692.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4631.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4582.jpg',
+              width: 4896,
+              height: 3264
+            },
           ]}
         />
         <Slideshow 
@@ -206,80 +202,76 @@ export default function Framo() {
           ]}
         />
         <ImagesCase 
+          rows={{
+            initial: [3,4,3,3],
+            bp1: [2,2,3,2],
+          }}
           images={[
-            [
-              {
-                src: '/img/seading/DSCF4579.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4472.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/DSCF4562.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ],
-            [
-              {
-                src: '/img/seading/_DSF3432.jpg',
-                width: 3264,
-                height: 4896
-              },
-              {
-                src: '/img/seading/_DSF3410.jpg',
-                width: 3264,
-                height: 4896
-              },
-              {
-                src: '/img/seading/_DSF3404.jpg',
-                width: 3264,
-                height: 4896
-              },
-              {
-                src: '/img/seading/_DSF3402.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ],
-            [
-              {
-                src: '/img/seading/_DSF3370.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/_DSF3296.jpg',
-                width: 4896,
-                height: 3264
-              },
-              {
-                src: '/img/seading/_DSF3298.jpg',
-                width: 4896,
-                height: 3264
-              },
-            ],
-            [
-              {
-                src: '/img/seading/_DSC0763.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/_DSC0761.jpg',
-                width: 4898,
-                height: 3265
-              },
-              {
-                src: '/img/seading/_DSC0747.jpg',
-                width: 4608,
-                height: 3072
-              },
-            ],
+            {
+              src: '/img/seading/DSCF4579.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4472.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/DSCF4562.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/_DSF3432.jpg',
+              width: 3264,
+              height: 4896
+            },
+            {
+              src: '/img/seading/_DSF3410.jpg',
+              width: 3264,
+              height: 4896
+            },
+            {
+              src: '/img/seading/_DSF3404.jpg',
+              width: 3264,
+              height: 4896
+            },
+            {
+              src: '/img/seading/_DSF3402.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/_DSF3370.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/_DSF3296.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/_DSF3298.jpg',
+              width: 4896,
+              height: 3264
+            },
+            {
+              src: '/img/seading/_DSC0763.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/_DSC0761.jpg',
+              width: 4898,
+              height: 3265
+            },
+            {
+              src: '/img/seading/_DSC0747.jpg',
+              width: 4608,
+              height: 3072
+            },
           ]}
         />
       </Box>
