9f7ca1c3424a3036d8e1aa9cd894d2134c64ed70
Julian Furchert <julian.furchert@googlemail.com>
2023-03-09 00:54:47 +0100

    test

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,9 +8,11 @@ export default function About() {
       </Text>
       <Box css={{ maxWidth: 1200 }}>
         <Paragraph>
-          I'm a Software Engineer based in Düsseldorf, and I have a soft spot in design systems and user interfaces. With over 6 years of experience in both design and development, I bring a unique and holistic perspective to creating digital products. My ability to comprehend both the technical and creative aspects of the development process enables me to develop elegant and effective solutions that meet the needs of users and business goals. I am interested in a wide range of subjects including web development, creative coding, design systems and interaction design.         </Paragraph>
+          I'm a Software Engineer based in Düsseldorf, and I have a soft spot in design systems and user interfaces. With over 6 years of experience in both design and development, I bring a unique and holistic perspective to creating digital products. My ability to comprehend both the technical and creative aspects of the development process enables me to develop elegant and effective solutions that meet the needs of users and business goals. I am interested in a wide range of subjects including web development, creative coding, design systems and interaction design.         
+        </Paragraph>
         <Paragraph>
-          Currently, I work at Raytion and lead the development of a component library for search applications. My responsibilities include conception, design, and development of this library. Not only does it serve our own product, but it also supports custom interfaces for various clients.        </Paragraph>
+          In my current role at Raytion, I am leading the development of a component library designed specifically for search applications. I am responsible for the conception, design, and development of this library, which not only serves our own product but also provides support for custom interfaces for our various clients.        
+        </Paragraph>
       </Box>
     </Container>
   )
