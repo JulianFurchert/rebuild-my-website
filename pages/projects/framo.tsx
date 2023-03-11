9b478dd2ea7088801d9a170ae06d5e9163a8944c
Julian Furchert <julian.furchert@googlemail.com>
2023-03-11 03:16:11 +0100

    Update framo

--- pages/projects/framo.tsx
+++ pages/projects/framo.tsx
@@ -7,10 +7,11 @@ export default function Page() {
       title="Framo"
       image="/og-img/og-image-framo.png"
       imageTwitter="/og-img/twitter-image-framo.png"
-      description='A website was developed and a responsive logo was designed for the London-based architecture firm Framo.'
-      text="A website was developed and a responsive logo was designed for the London-based architecture firm Framo. The logo consists of six letters that adapt to the available space depending on the width of the screen. The shape of the letters always changes with the aspect ratio of the screen. To make the logo usable in other media, a web app was developed in which the letters can be freely arranged and scaled. The result can be saved as a vector file. Both website and app are implemented with React. Design and conception of the website are by Mind Design."
+      description='Website and logo for Framo, an architectural practice based in East London. '
+      text="Website and logo for Framo, an architectural practice based in East London. The logo contains six letters. The shape of the letters is dynamic and adapts to the aspect ratio of the available space on the screen. A web tool was developed to make the logo also usable in other media. The result can be saved as a vector file. Website and development of the responsive logo was done in cooperation with Mind Design."
       links={[
-        { name: 'Site', url: 'https://www.framo.studio/' }
+        { name: 'Site', url: 'https://www.framo.studio/' },
+        { name: 'Tool', url: 'https://framo-playground.netlify.app/' }
       ]}
       stack={[ 'React', 'Styled Components', 'React Pose', 'Snap.svg', 'Netlify', 'Prismic' ]}
     >
