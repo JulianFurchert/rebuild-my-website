4572bb718794a6efb4f294bbca581359ac8d75df
Julian Furchert <julian.furchert@googlemail.com>
2023-03-11 13:45:41 +0100

    Update reprofront

--- pages/projects/reprofont.tsx
+++ pages/projects/reprofont.tsx
@@ -7,8 +7,8 @@ export default function Page() {
       title="Repro Font"
       image="/og-img/og-image-repro.png"
       imageTwitter="/og-img/twitter-image-repro.png"
-      description="The Repro typeface consists of six weights, all generated using an algorithm."
-      text="The Repro typeface consists of six weights, all generated using an algorithm. The idea for the project and the algorithm was born while working in Cinema4D, where text paths and splines are recalculated for export. There are several interpolation methods available that influence the appearance of the exported glyphs. This inspired me to develop my own method for recalculating glyphs, which creates intermediate points along the paths and moves them onto a virtual orthogonal grid. Repro Null serves as the base for the other font weights. The grid becomes coarser from weight to weight until the resulting characters are no longer legible."
+      description="The Repro typeface is made up of seven weights, of which five were created using an algorithm."
+      text="The Repro typeface is made up of seven weights, of which five were created using an algorithm. The idea for the project was born while working in Cinema4D, where text paths and splines are recalculated for export. There are several interpolation methods available that influence the appearance of the exported glyphs. This inspired me to develop my own method for recalculating glyphs. The method creates intermediate points along the paths and moves them onto a virtual orthogonal grid. Repro Null serves as the base for the other font weights. The grid becomes coarser from weight to weight until the resulting characters are no longer readable."
       links={[
         { name: 'Site', url: 'https://reprofont.de/' },
         { name: 'Code', url: 'https://github.com/JulianFurchert/reprofont-website' },
